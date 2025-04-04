53cbcbbd5d24b0fc4b248769bfd0c0dd423ca83e
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2025-04-03 23:41:19 -0400

    chore: Bump version to 2.0.6 for Strava and Garmin Kudos All script

--- KudoAll-Strava-Garmin.user.js
+++ KudoAll-Strava-Garmin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Strava and Garmin Kudos All (Working)
 // @namespace    typpi.online
-// @version      2.0.5
+// @version      2.0.6
 // @description  Adds a button to give kudos to all visible activities on Strava and Garmin Connect.
 // @author       Nick2bad4u
 // @license      Unlicense
