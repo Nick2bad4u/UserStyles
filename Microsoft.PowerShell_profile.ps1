c792689e1839ac36c9e149a24a4471daeffc9c3b
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2025-05-25 12:03:20 -0400

    chore: simplify sitemap workflow trigger and remove scheduling

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,3 +7,8 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
